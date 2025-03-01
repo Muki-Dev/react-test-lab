c22f1bf1c1b46d352d8f3e68e4226731b64a2a23
muki Dev <mukidev881@gmail.com>
2025-03-01 23:05:23 +0000

    add style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
  import React, { useState } from 'react';
  import Header from './components/Header';
  import Button from './components/Button';
+ import './App.css';
 
  function App() {
   const [count,setCount] = useState(0);
 
   return(
-      <div>
+      <div className='container'>
         <Header title='Bienvenue sur Testify React ' />
         <p>Compteur : { count } </p>
         <Button label='Increment' onClick={() => setCount(count + 1)} />
